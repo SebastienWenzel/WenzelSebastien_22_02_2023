bf0a395585e1e4296d51fb74a68530a9caf59f08
senik <sebastienwenzel@gmail.com>
2023-02-22 14:59:38 +1100

    install express, mongoose  and set up Server js

---
+++ back/server.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const app = require('./app');
+
+/*
+ * Normalise le numéro du port
+ * @param {*} val 
+ * @returns 
+ */
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/*
+ * Permet de gérer les erreur de connection au port du backend en cas de port déjà utilisé
+ * @param {*} error 
+ */
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+
+/*
+Permet au server d'écouter sur le port disponible
+ */
+
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
